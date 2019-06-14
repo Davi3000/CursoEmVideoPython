1b34d06aa57c8168329c5966d5ec4115d5698f9d
Davi3000 <nearwiner1234567@gmail.com>
2019-06-14 00:23:21 -0300

    FASE08CONCLUIDA

--- Desafio021.py
+++ Desafio021.py
@@ -1,5 +1,10 @@
-import pygame
+'''import pygame
 pygame.init()
 pygame.mixer.music.load('bnha3.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+pygame.event.wait()'''
+from pygame import mixer
+mixer.init()
+mixer.music.load('bnha3.mp3')
+mixer.music.play()
+input('Agora você escuta?')
